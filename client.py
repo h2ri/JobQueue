699653327e70134f78f815246c3c80d006343564
Healthi <healthi@Healthis-MacBook-Pro.local>
2017-11-21 19:08:54 +0530

    process and memory limit to be set while calling the application

--- client.py
+++ client.py
@@ -14,31 +14,14 @@ NUM_TASKS = 3
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
-def do_something(arg1, arg2):
-
-    #container.stop()
-    print("Inside mp_worker")
-    print(os.getpid())
-    logger.info("Performing task with arg1=%s and arg2=%s", arg1, arg2)
-    time.sleep(5)
-    print("Done")
-
 def main_client():
-
-    # r = redis.Redis(
-    #     host='localhost',
-    #     port=6379
-    # )
     queue_name = 'tasaks' #replace with args.queue
     #redis_conn = redis_queue.get_redis_conn(host=args.host, port=args.port, db=args.db)
     redis_conn = redis_queue.get_redis_conn()
     redis_queue_q = redis_queue.RedisQueue(queue_name, redis_conn)
-
     logger.info("Generating %i tasks", NUM_TASKS)
-
     for i in range(NUM_TASKS):
-        a = job.Job(name="hari21/hello:1.0", memory={"mem" : "50M"})
+        a = job.Job(name="hari21/hello:1.0", memory={"memory" : "8"})
         #a = job.Job('hari21/hello:1.0')
         data = dill.dumps(a)
         redis_queue_q.add(data)
--- worker.py
+++ worker.py
@@ -7,31 +7,38 @@ import args
 import redis_queue
 import job
 import dill
+import resource
+import sys
 
 the_queue = multiprocessing.Queue()
 
-def worker_main(queue):
+
+def worker_main(queue,mem_limit_mb):
+    resource.setrlimit(resource.RLIMIT_STACK, ( mem_limit_mb * 1024 * 100 * 10,
+     mem_limit_mb * 1024 * 100 * 10)) #TO set limit of in kb
+    soft, hard = resource.getrlimit(resource.RLIMIT_STACK)
+    alloted_memory = hard / 1024 / 1024
     print os.getpid(),"working"
     while True:
         item = queue.get(True)
         if item:
-            print os.getpid(), "got", item
             time.sleep(5) # simulate a "long" operation
             job_data = dill.loads(item)
-            print(job_data)
             client = docker.from_env()
             try:
-                container = client.containers.run(job_data.container_name, detach=True, mem_limit="50M")
-                while container.status != 'exited':
-                    time.sleep(5)
-                    container.reload()
-                    print("Inside contianer")
-                print("Done")
+                if int(job_data.memory_requirment["memory"]) <= alloted_memory:
+                    container = client.containers.run(job_data.container_name, detach=True, mem_limit="50M")
+                    while container.status != 'exited':
+                        time.sleep(5)
+                        container.reload()
+                    print("Job Completed")
+                else:
+                    print("Memory not available to process the request")
             except docker.errors.NotFound:
                 print("File Not Found")
 
-def mp_handler():
-    the_pool = multiprocessing.Pool(3, worker_main,(the_queue,))
+def mp_handler(no_of_process, mem_limit):
+    the_pool = multiprocessing.Pool(no_of_process, worker_main,(the_queue,mem_limit))
 
 
 if __name__ == '__main__':
@@ -39,6 +46,6 @@ if __name__ == '__main__':
     #redis_conn = redis_queue.get_redis_conn(host=args.host, port=args.port, db=args.db)
     redis_conn = redis_queue.get_redis_conn()
     redis_queue_q = redis_queue.RedisQueue(queue_name, redis_conn)
-    mp_handler()
+    mp_handler(int(sys.argv[1]),int(sys.argv[2]))
     while True:
         the_queue.put(redis_queue_q.remove())
